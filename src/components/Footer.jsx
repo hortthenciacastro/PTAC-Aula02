e5a703d941f2456817ef6187f9493d7bccb674f5
hortthenciacastro <hortthencia.castro@estudante.ifms.edu.br>
2024-03-01 09:51:41 -0400

    criação do footer
    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+    return (
+        <footer>
+        <p>Desenvolvedor: Hortthencia Castro</p>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header () {
+    return (
+        <header> <h2>Informática</h2></header>
+    )
+}

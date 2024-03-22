3f3ec5936e1852fe7e7586ee976b44bf10310907
hortthenciacastro <hortthencia.castro@estudante.ifms.edu.br>
2024-03-22 09:45:47 -0400

    22/03
    lista telefonica

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,32 @@ import { useState } from "react";
 
 export default function Main(){
     const[nome, setNome] = useState("");
+    const[telefone, setTelefone] = useState("");
     
     return(
         <main>
+            <form>
+                <label> nome
             <input
             type="text"
-            name=""
-            id=""
+            name="nome-contato"
+            id="nome"
             onChange={(event)=> setNome(event.target.value)}
             />
+            </label>
             {nome}
+
+           <label> telefone
+            <input
+            type="tel"
+            name="telefone-contato"
+            id="telefone"
+            onChange={(event)=> setTelefone(event.target.value)}
+            />
+            </label>
+            {telefone}
+            <button> Enviar </button>
+            </form>
         </main>
     )
 }
